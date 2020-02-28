7e7abf5398e2ba985ee004e298abde5b1f116be8
dvasdekis <dimitri@vasdekis.com.au>
2020-02-28 14:49:50 +1100

    L15

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 function Body() {
+    const firstName = "Dimitri"
+    const lastName = "Vasdekis"
+    const date = new Date()
     return (
         <main>
-            <p>My Body</p>
+            <p>Hello {firstName + " " + lastName} !!</p>
+            <h4>Hello {`${firstName} ${lastName}`}!!!</h4> {/* Different syntax */}
+            <p>Current time is {date.getHours() % 12} o'clock!</p>
         </main>
     )
 }
