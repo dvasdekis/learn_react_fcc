58679c965c60d28f0bc6c8410abcf4f7f7fe0b84
dvasdekis <dimitri@vasdekis.com.au>
2020-02-28 15:05:08 +1100

    L16

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,10 +4,16 @@ function Body() {
     const firstName = "Dimitri"
     const lastName = "Vasdekis"
     const date = new Date()
+
+    const pStyle = {
+        color: "#AF8C00", backgroundColor: "#AF2D00"
+    }
     return (
         <main>
-            <p>Hello {firstName + " " + lastName} !!</p>
-            <h4>Hello {`${firstName} ${lastName}`}!!!</h4> {/* Different syntax */}
+            {/* Preferred syntax */}
+            <p style={pStyle}>Hello {firstName + " " + lastName} !!</p>
+            {/* Alternative syntax */}
+            <h4 style={{color: "#FF8C00", backgroundColor: "#FF2D00"}}>Hello {`${firstName} ${lastName}`}!!!</h4>
             <p>Current time is {date.getHours() % 12} o'clock!</p>
         </main>
     )
