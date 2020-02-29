e3871086d43c51fe92231b292eb18351ada87ad3
dvasdekis <dimitri@vasdekis.com.au>
2020-02-29 13:07:39 +1100

    Empty project

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# learn_react_fcc
-Course from https://www.youtube.com/watch?v=DLX62G4lc44
--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Header() {
-    return (
-        <header className={"navbar"}>this is my header</header>
-    )
-}
-
-export default Header
