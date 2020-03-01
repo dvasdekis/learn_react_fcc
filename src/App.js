2ef7d6829edbccabe0251f8549de3be856859a9d
dvasdekis <dimitri@vasdekis.com.au>
2020-03-01 17:14:46 +1100

    Working L26

--- src/App.js
+++ src/App.js
@@ -2,23 +2,26 @@ import React from 'react';
 import productsData from "./vsProducts"
 import Product from "./components/Product"
 
-function App() {
-  const productset = productsData.map(function(eachProduct) {
-      return (
-          <Product
-            key = {eachProduct.id}
-            id={eachProduct.id}
-            name = {eachProduct.name}
-            description = {eachProduct.description}
-            price = {eachProduct.price}
-      /> )
-  })
+class App extends React.Component {
+    // Every class-based component needs the Render method, in lower case (exact spelling)
+    render() { // In here, this can work exactly like the old App() function
+        const productset = productsData.map(function(eachProduct) {
+            return (
+                <Product
+                    key = {eachProduct.id}
+                    id={eachProduct.id}
+                    name = {eachProduct.name}
+                    description = {eachProduct.description}
+                    price = {eachProduct.price}
+                /> )
+        })
 
-  return (
-    <div className="App">
-        {productset}
-    </div>
-  );
+        return (
+            <div className="App">
+                {productset}
+            </div>
+        )
+    }
 }
 
 export default App;
